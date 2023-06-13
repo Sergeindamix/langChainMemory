ca16ce3912ba381cdf339a5e9cb5555f3d7d5294
Sergeindamix <86927266+Sergeindamix@users.noreply.github.com>
2023-06-13 15:11:29 -0600

    Update AgentTools.py

--- AgentTools.py
+++ AgentTools.py
@@ -8,3 +8,15 @@ tools = [
         description="useful for when you need to answer questions about current events"
     )
 ]
+
+def duck_wrapper(input_text):
+    search_results = search.run(f"site:webmd.com {input_text}")
+    return search_results
+
+tools = [
+    Tool(
+        name = "Search WebMD",
+        func=duck_wrapper,
+        description="useful for when you need to answer medical and pharmalogical questions"
+    )
+]
